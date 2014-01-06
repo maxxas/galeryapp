07efb972bba13b36a13562df3afa2ccbccedb22e
maxxas <maxschamber@gmail.com>
2014-01-06 21:55:24 +0100

    uebung fersch

---
+++ app/models/painting.rb
@@ -0,0 +1,5 @@
+class Painting < ActiveRecord::Base
+
+	mount_uploader :image, ImageUploader
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galeryapp::Application.config.secret_key_base = 'd00edde0a4132a110e11a47e0ec1ba319c2cf55a74332851ea691990a6ca36695d9e6ab0fd16ae6c8a29cec6713192decd6df0f63161984f18a3ead33f10ad3a'
